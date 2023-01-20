104c7120fe7a6184e8ba86a19a24b23f15ced947
Owen Jeffreys <osjeffreys.uk@gmail.com>
2023-01-20 22:47:43 +0000

    tidy up

--- sw/esp32/main/main.c
+++ sw/esp32/main/main.c
@@ -27,9 +27,9 @@
  */
 struct smart_bulb_state
 {
-    bool    on_off;         /*!< Current on/off state of the smartbulb */
-    uint8_t brightness;     /*!< Current brightness (in percent) of the smartbulb */
-    struct  hsv_colour hsv; /*!< Current colour (in HSV) of the smartbulb */
+    bool    on_off;            /*!< Current on/off state of the smartbulb */
+    uint8_t brightness;        /*!< Current brightness (in percent) of the smartbulb */
+    struct  rgb_colour colour; /*!< Current colour (in RGB) of the smartbulb */
 } current_state;
 
 /**
@@ -41,26 +41,27 @@ void app_main(void)
     const char *log_tag = "ble-iot-bridge";
 
     /* connect to the configured WiFi network */
-    ESP_LOGI(log_tag, "Conecting to WiFi");
+    ESP_LOGI(log_tag, "Conecting to WiFi network");
     wifi_connect();
 
-    /* wait till network is ready */
+    /* wait till network is ready and start TCP server */
     while ( !wifi_network_ready() )
     {
         vTaskDelay(500 / portTICK_RATE_MS);
     }
+    ESP_LOGI(log_tag, "Starting WiFi smartbulb emulator (TCP server port 9999)");
+    wifi_start_server();
 
     /* start bluetooth and connect to BLE smartbulb */
     bluetooth_start();
 
-    /* main loop */
-    ESP_LOGI(log_tag, "Starting server");
-    wifi_start_server();
-    
+    /* main loop to bridge WiFi and BLE */
     while (true)
     {
-        struct rgb_colour rgb = { rand() % 256, rand() % 256, rand() % 256 };
-        bluetooth_set_bulb_colour(rgb);
+        current_state.colour.r = rand() % 256;
+        current_state.colour.g = rand() % 256;
+        current_state.colour.b = rand() % 256;
+        bluetooth_set_bulb_colour(current_state.colour);
         vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
